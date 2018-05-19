9b70617e735a0cd5d20604a5e041b2e803afe4ed
Julius Sandgren <iamdjulz@gmail.com>
2018-05-19 17:31:36 +0200

    test

--- NodejsWebApp2/public/js/chat.js
+++ NodejsWebApp2/public/js/chat.js
@@ -1,10 +1,19 @@
 var webrtc = new SimpleWebRTC({
     // the id/element dom element that will hold "our" video
-    localVideoEl: 'localVideo',
+    localVideoEl: '',
     // the id/element dom element that will hold remote videos
-    remoteVideosEl: 'remoteVideos',
+    remoteVideosEl: '',
     // immediately ask for camera access
-    autoRequestMedia: true
+    autoRequestMedia: true,
+    enableDataChannels: false,
+    media: {
+        audio: true,
+        video: false
+    },
+    receiveMedia: { // FIXME: remove old chrome <= 37 constraints format
+        offerToReceiveAudio: 1,
+        offerToReceiveVideo: 0
+    },
 });
 
 webrtc.on('readyToCall', function () {
